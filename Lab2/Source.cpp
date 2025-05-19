ff17482721a305ad64401a36b302477817ee677f
Bach <bakhtuev.v.a@yandex.ru>
2025-05-19 22:47:29 +0300

    new int

--- Lab2/Source.cpp
+++ Lab2/Source.cpp
@@ -6,8 +6,8 @@ using namespace std;
 struct Book {
     string author;
     string title;
-    int year;
-    int pages;
+    int year1;
+    int pages1;
 };
 
 void fillBooks(Book books[], int& count) {
@@ -22,9 +22,9 @@ void fillBooks(Book books[], int& count) {
         cout << "Название: ";
         cin >> books[i].title;
         cout << "Год издания: ";
-        cin >> books[i].year;
+        cin >> books[i].year1;
         cout << "Число страниц: ";
-        cin >> books[i].pages;
+        cin >> books[i].pages1;
         cin.ignore(); 
     }
 }
@@ -34,8 +34,8 @@ void outBooks(const Book books[], int count) {
     for (int i = 0; i < count; i++) {
         cout << "Автор: " << books[i].author << endl 
              << "Название: " << books[i].title << endl 
-             << "Год издания: " << books[i].year << endl 
-             << "Число страниц: " << books[i].pages << endl;
+             << "Год издания: " << books[i].year1 << endl 
+             << "Число страниц: " << books[i].pages1 << endl;
         
     }
 }
@@ -53,7 +53,7 @@ int countBooksAuthor(const Book books[], int count, string author) {
 Book findLastBook(const Book books[], int count) {
     Book latest = books[0];
     for (int i = 1; i < count; i++) {
-        if (books[i].year > latest.year) {
+        if (books[i].year1 > latest.year1) {
             latest = books[i];
         }
     }
@@ -63,21 +63,21 @@ Book findLastBook(const Book books[], int count) {
 int main1() {
     const int Max_B = 100;
     Book books[Max_B];
-    int count;
+    int count1;
 
-    fillBooks(books, count);
-    outBooks(books, count);
+    fillBooks(books, count1);
+    outBooks(books, count1);
 
     
     string author;
     cout << "\nВведите имя автора для поиска количества его книг: ";
     cin >> author;
-    cout << "Количество книг автора " << author << ": " << countBooksAuthor(books, count, author) << endl;
+    cout << "Количество книг автора " << author << ": " << countBooksAuthor(books, count1, author) << endl;
     
-    Book last = findLastBook(books, count);
+    Book last = findLastBook(books, count1);
     cout << "\nКнига с самым поздним годом издания:\n";
     cout << "Автор: " << last.author << ", Название: " << last.title
-        << ", Год издания: " << last.year << ", Число страниц: " << last.pages << endl;
+        << ", Год издания: " << last.year1 << ", Число страниц: " << last.pages1 << endl;
 
     return 0;
 }
